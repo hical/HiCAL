f055527931002a70b0e5985f0d74ad1fd5d45cdb
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-05-04 14:40:47 -0400

    Add build script

---
+++ CALEngine/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -O2 -pthread src/bmi_cli.cc src/scorer.cc src/sofiaml/sf-sparse-vector.cc src/sofiaml/sf-data-set.cc src/sofiaml/sf-weight-vector.cc src/sofiaml/sofia-ml-methods.cc -o bmi_cli
