1b8033b4a3a07864f2d26e572ee5cab48892c1ee
Mustafa Abualsaud <mabualsaud@outlook.com>
2017-06-30 13:17:36 -0400

    Reformat latin square generator

--- TrecCoreWeb/treccoreweb/sequence/generateLatinSquare.py
+++ TrecCoreWeb/treccoreweb/sequence/generateLatinSquare.py
@@ -6,93 +6,76 @@
 
 __revision__ = '0.1'
 
-import sys,os
-import getopt
 import numpy as np
 import json
-import random
-import scipy.stats as stats
-# import matplotlib.pyplot as plt
-# import matplotlib.ticker as mtick
-# from matplotlib.backends.backend_pdf import PdfPages
-import collections
-import json
 
-LatinSquare= np.array([\
-	["A1","B2","C3","D4","E5"],\
-	["C2","D3","E4","A5","B1"],\
-	['E3','A4','B5','C1','D2'],\
-	['B4','C5','D1','E2','A3'],\
-	['D5','E1','A2','B3','C4']])
+LatinSquare = np.array([["A1", "B2", "C3", "D4", "E5"], ["C2", "D3", "E4", "A5", "B1"],
+                        ['E3', 'A4', 'B5', 'C1', 'D2'], ['B4', 'C5', 'D1', 'E2', 'A3'],
+                        ['D5', 'E1', 'A2', 'B3', 'C4']])
 
 with open("nist.topics") as f:
     nistTopics = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
-nistTopicList = [int(x.strip()) for x in nistTopics] 
+nistTopicList = [int(x.strip()) for x in nistTopics]
 
 # print nistTopicList
 
 np.random.seed(1234)
 
-topicDict ={}
-interfaceDict ={}
+topicDict = {}
+interfaceDict = {}
 for i in range(10):
-	np.random.shuffle(LatinSquare) 
-	np.random.shuffle(np.transpose(LatinSquare))
+    np.random.shuffle(LatinSquare)
+    np.random.shuffle(np.transpose(LatinSquare))
 
-	# print LatinSquare
-	
-	j = 0
-	for row in np.asarray(LatinSquare):
-		seqList = []
-		topicList = []
-		for item in row:
-			# print item[0]
-			seqList.append(ord(item[0])-ord('A')+1)
-			topicList.append(i*5 + int(item[1]))
-		# print firstList
-		# print secondList
-		interfaceDict[i+j*10] = seqList
-		topicDict[i+j*10] = topicList
-		j += 1
+    # print LatinSquare
 
+    j = 0
+    for row in np.asarray(LatinSquare):
+        seqList = []
+        topicList = []
+        for item in row:
+            # print item[0]
+            seqList.append(ord(item[0]) - ord('A') + 1)
+            topicList.append(i * 5 + int(item[1]))
+        # print firstList
+        # print secondList
+        interfaceDict[i + j * 10] = seqList
+        topicDict[i + j * 10] = topicList
+        j += 1
 
-treatmentDict = {}
-treatmentDict[1] =["true","true","false","true" ]
-treatmentDict[2] =["true","false","false","true" ]
-treatmentDict[3] =["false","true","false","true" ]
-treatmentDict[4] =["false","false","false","true" ]
-treatmentDict[5] =["false","false","true","true" ]
-
+treatmentDict = {1: [True, True, False, True],
+                 2: [True, False, False, True],
+                 3: [False, True, False, True],
+                 4: [False, False, False, True],
+                 5: [False, False, True, True]}
 
 treatmentDumps = []
-for i in range(0,50):
-	# print i
-	aList = topicDict[i]
-	# print aList
-	topicList = []
-	for t in aList:
-		topicList.append(nistTopicList[t-1])
-	# print topicList
+for i in range(0, 50):
+    # print i
+    aList = topicDict[i]
+    # print aList
+    topicList = []
+    for t in aList:
+        topicList.append(nistTopicList[t - 1])
+    # print topicList
 
-	bList = interfaceDict[i]
-	# print bList
-	interfaceList = []
-	for interface in bList:
-		interfaceList.append(treatmentDict[interface])
+    bList = interfaceDict[i]
+    # print bList
+    interfaceList = []
+    for interface in bList:
+        interfaceList.append(treatmentDict[interface])
 
-	# print interfaceList
-	treatment = {"user_ID":i,\
-     "treatments":[{\
-     'topic_num':topic,\
-     "setting":[{\
-     "show_search":interface[0],\
-     "toggle_doc":interface[1],\
-     "only_show_doc":interface[2],\
-     "show_cal":interface[3],\
-     }]} for topic,interface in zip(topicList, interfaceList)]}
+    # print interfaceList
+    treatment = {"user_ID": i, \
+                 "treatments": [
+                     dict(topic_num=topic, setting=dict(show_search=interface[0],
+                                                         toggle_doc=interface[1],
+                                                         only_show_doc=interface[2],
+                                                         show_cal=interface[3])) for
+                     topic, interface in
+                     zip(topicList, interfaceList)]}
 
+    treatmentDumps.append(treatment)
 
-   	treatmentDumps.append(treatment)
-
-print json.dumps(treatmentDumps, indent=4)
+print(json.dumps(treatmentDumps, indent=4))
