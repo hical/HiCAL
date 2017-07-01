afbeb1edd3bd68507e8ad6602ffa42b936f0fbc9
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-06-30 20:06:52 -0400

    Fix migrations

--- TrecCoreWeb/config/settings/base.py
+++ TrecCoreWeb/config/settings/base.py
@@ -115,7 +115,7 @@ MANAGERS = ADMINS
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres:///treccoreweb'),
+    'default': env.db('DATABASE_URL', default='postgres:///treccore2'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
