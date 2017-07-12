e4b5b8289f486adcd9b04ff28b97aeb045466269
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-07-12 19:03:43 -0400

    Replace blacklisting with whitelisting

--- TrecCoreWeb/treccoreweb/middleware/timer.py
+++ TrecCoreWeb/treccoreweb/middleware/timer.py
@@ -1,27 +1,42 @@
 import time
-from datetime import datetime
 from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
+from django.urls import reverse
 from config.settings.base import INACTIVE_TRIGGER_TIME
 
-REDIRECT_EXCEPTION = [
-    reverse_lazy('progress:completed'),
-    reverse_lazy('progress:posttask')
+ACTIVE_URLS = [
+    reverse('CAL:post_log_msg'),
+    reverse('CAL:get_docs'),
+    reverse('CAL:main'),
+    reverse('iterative:post_log_msg'),
+    reverse('iterative:get_docs'),
+    reverse('judgment:post_judgment'),
+    reverse('judgment:post_nojudgment'),
+    reverse('progress:post_ctrlf'),
+    reverse('progress:post_find_keystroke'),
+    reverse('progress:post_visit'),
+    reverse('search:main'),
+    reverse('search:get_docs'),
+    reverse('search:get_doc'),
+    reverse('search:post_search_status'),
+    reverse('search:post_keystroke')
 ]
 
 
 def timer_middleware(get_response):
     def middleware(request):
-        cur_time = time.time()
-        prev_time = request.user.current_task.last_activity
-        if prev_time == None:
-            prev_time = cur_time
-        request.user.current_task.timespent += min(cur_time - prev_time, INACTIVE_TRIGGER_TIME)
-        request.user.current_task.last_activity = cur_time
-        request.user.current_task.save()
+        for url_exception in ACTIVE_URLS:
+            if request.path.startswith(url_exception):
+                cur_time = time.time()
+                prev_time = request.user.current_task.last_activity
+                if prev_time == None:
+                    prev_time = cur_time
+                request.user.current_task.timespent += min(cur_time - prev_time, INACTIVE_TRIGGER_TIME)
+                request.user.current_task.last_activity = cur_time
+                request.user.current_task.save()
 
-        if request.user.current_task.is_time_past() and request.path not in REDIRECT_EXCEPTION:
-            return HttpResponseRedirect(reverse_lazy('progress:completed'))
+                if request.user.current_task.is_time_past():
+                    return HttpResponseRedirect(reverse_lazy('progress:completed'))
+                break
 
         response = get_response(request)
         return response
