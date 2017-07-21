7e766e47cd14c784473c07ec2e0efedbc65012e3
Mustafa Abualsaud <mabualsaud@outlook.com>
2017-07-21 16:46:21 -0400

    Update judgement command

--- TrecCoreWeb/treccoreweb/judgment/management/commands/judgments_to_csv.py
+++ TrecCoreWeb/treccoreweb/judgment/management/commands/judgments_to_csv.py
@@ -8,7 +8,8 @@ from treccoreweb.judgment.models import Judgement
 class Command(BaseCommand):
     help = 'Export judgments to csv format'
     filename = 'judgments.csv'
-    header = ('USER', 'TASK', 'TOPIC', 'DOCID', 'METHOD', 'JUDGMENT', 'TIME_TO_JUDGE')
+    header = ('USER', 'TASK', 'TOPIC', 'DOCID', 'METHOD', 'JUDGMENT', 'TIME_AWAY',
+              'TIME_TO_JUDGE')
 
     def handle(self, *args, **option):
         self.stdout.write(self.style.SUCCESS("Writing to "
@@ -20,8 +21,9 @@ class Command(BaseCommand):
             for judgment in judgments:
                 value = 2 if judgment.highlyRelevant else 1 if judgment.relevant else 0 if judgment.nonrelevant else None
                 # if value is empty, then user looked at document but not has not judged
-                if not value:
+                if value is None:
                     continue
+
                 user = judgment.user
                 task = judgment.task.setting
                 topic = judgment.task.topic.number
@@ -37,13 +39,17 @@ class Command(BaseCommand):
                 else:
                     method = None
                 time_to_judge = 0.0
+                time_away = 0.0
                 for d in judgment.timeVerbose:
                     # judgments from search SERP don't have a time counter, set to 0.0
                     if d.get('source') == 'searchSERP':
                         continue
                     time_to_judge += d.get('timeActive')
+                    time_away += d.get('timeAway')
+                time_away /= 1000
                 time_to_judge /= 1000
-                writer.writerow((user, task, topic, docid, method, value, time_to_judge))
+                writer.writerow((user, task, topic, docid, method,
+                                 value, time_away, time_to_judge))
 
         self.stdout.write(self.style.SUCCESS(
             'Successfully exported judgments to {}'.format(self.filename)))
