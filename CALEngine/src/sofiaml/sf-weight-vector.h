138d0953ea67e49db6ae9e0f2aee8fb3669a198e
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-10-20 15:32:57 -0400

    Remove unneeded code

--- CALEngine/src/sofiaml/sf-weight-vector.h
+++ CALEngine/src/sofiaml/sf-weight-vector.h
@@ -43,21 +43,10 @@ class SfWeightVector {
   // zero, scale_ set to 1, and squared_norm_ set to 0.
   SfWeightVector(int dimensionality);
 
-  // Constructs a weight vector from a string, which is identical in format
-  // to that produced by the AsString() member method.
-  SfWeightVector(const string& weight_vector_string);
-
-  // Simple copy constructor, needed to allocate a new array of weights.
-  SfWeightVector(const SfWeightVector& weight_vector);
-
-  // Frees the array of weights.
+    // Frees the array of weights.
   virtual ~SfWeightVector();
 
-  // Re-scales weight vector to scale of 1, and then outputs each weight in
-  // order, space separated.
-  string AsString();
-
-  vector<float> AsFloatVector();
+    vector<float> AsFloatVector();
 
   // Computes inner product of <x_scale * x, w>
   virtual float InnerProduct(const SfSparseVector& x,
@@ -74,21 +63,10 @@ class SfWeightVector {
   // w *= scaling_factor
   void ScaleBy(double scaling_factor);
 
-  // Returns value of element w_index, taking internal scaling into account.
-  float ValueOf(int index) const;
-
-  // Project this vector into the L1 ball of radius lambda.
-  void ProjectToL1Ball(float lambda);
-
-  // Project this vector into the L1 ball of radius at most lambda, plus or
-  // minus epsilon / 2.
-  void ProjectToL1Ball(float lambda, float epsilon);
-  
-  // Getters.
+    // Getters.
   double GetSquaredNorm() const { return squared_norm_; }
-  int GetDimensions() const { return dimensions_; }
 
- protected:
+protected:
   void ScaleToOne();
 
   float* weights_;
