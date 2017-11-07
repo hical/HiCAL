5f2426867e9d3fb68c3e5bdaa35c237ab25ad1a2
Mustafa Abualsaud <mabualsaud@outlook.com>
2017-11-06 19:41:38 -0500

    Update start.sh and add missing migrations

--- TrecCoreWeb/compose/local/django/start.sh
+++ TrecCoreWeb/compose/local/django/start.sh
@@ -7,4 +7,6 @@ set -o xtrace
 
 
 python manage.py migrate
+python manage.py loaddata treccoreweb/progress/fixtures/init_tasksetting.json
+python manage.py loaddata treccoreweb/topic/fixtures/init_topics.json
 python manage.py runserver 0.0.0.0:8000
