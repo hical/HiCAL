8afe4224a83ba9f693bb38b0a680974746e8e207
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-11-06 22:27:30 -0500

    Working docker

--- TrecCoreWeb/compose/local/django/start.sh
+++ TrecCoreWeb/compose/local/django/start.sh
@@ -10,7 +10,7 @@ python manage.py makemigrations && python manage.py migrate
 python manage.py loaddata treccoreweb/progress/fixtures/init_tasksetting.json
 python manage.py loaddata treccoreweb/topic/fixtures/init_topics.json
 python manage.py collectstatic --no-input
-uwsgi --socket 127.0.0.1:8001 --module config.wsgi --master --process 2 --threads 4
+uwsgi --socket 0.0.0.0:8001 --module config.wsgi --master --process 2 --threads 4
 
 
 
