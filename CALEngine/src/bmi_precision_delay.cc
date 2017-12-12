ee83db791fa89c9f764789054e1de7712886126c
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-12-12 12:40:31 -0500

    working precision delay

---
+++ CALEngine/src/bmi_precision_delay.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include "bmi_precision_delay.h"
+BMI_precision_delay::BMI_precision_delay(const Seed &_seed,
+        Dataset *_documents,
+        int _num_threads,
+        int _max_effort,
+        int _max_iterations,
+        bool _async_mode,
+        float _delta,
+        float _noise_factor)
+    :BMI(_seed, _documents, _num_threads, 10000, _max_effort, _max_iterations, _async_mode, false),
+    noise_factor(_noise_factor), delta(_delta)
+{
+    perform_iteration();
+    tot = 0;
+    rel = 0;
+    cur_p = 1.0;
+}
+
+void BMI_precision_delay::record_judgment_batch(std::vector<std::pair<std::string, int>> _judgments){
+    for(const auto &judgment: _judgments){
+        size_t id = documents->get_index(judgment.first);
+        add_to_training_cache(id, judgment.second);
+        remove_from_judgment_list(id);
+        tot+=1;
+        if(judgment.second > 0)
+            rel += 1;
+    }
+
+    if(!async_mode){
+        if(tot >= noise_factor / cur_p && (rel / tot < cur_p)){
+            std::cerr<<"Time to retrain: P="<<cur_p<<" tot="<<tot<<std::endl;
+            cur_p *= delta;
+            tot = rel = 0;
+            perform_iteration();
+        }
+    }else{
+        auto t = std::thread(&BMI_precision_delay::perform_iteration_async, this);
+        t.detach();
+    }
+}
---
+++ CALEngine/src/bmi_precision_delay.h
@@ -0,0 +1,24 @@
+#ifndef BMI_PRECISION_DELAY_H
+#define BMI_PRECISION_DELAY_H
+
+#include "bmi.h"
+
+class BMI_precision_delay:public BMI {
+    protected:
+    float delta, noise_factor;
+    float cur_p;
+    float tot, rel;
+    void record_judgment_batch(std::vector<std::pair<std::string, int>> _judgments);
+
+    public:
+    BMI_precision_delay(const Seed &seed,
+        Dataset *documents,
+        int num_threads,
+        int max_effort,
+        int max_iterations,
+        bool async_mode,
+        float delta,
+        float noise_factor);
+};
+
+#endif // BMI_PRECISION_DELAY_H
