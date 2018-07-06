75f46b6574ea8d616522a3e8df590af02b5b5d4a
Mustafa Abualsaud <mabualsaud@outlook.com>
2018-07-06 15:10:44 -0400

    Disable debug toolbar

--- HiCALWeb/config/settings/local.py
+++ HiCALWeb/config/settings/local.py
@@ -43,8 +43,8 @@ CACHES = {
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
-INSTALLED_APPS += ['debug_toolbar', ]
+# MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+# INSTALLED_APPS += ['debug_toolbar', ]
 
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
 
