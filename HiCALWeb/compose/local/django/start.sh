5351a2882c10d21fad2036ff811f27d08d87dcc8
Mustafa Abualsaud <mabualsaud@outlook.com>
2020-06-01 20:04:42 -0400

    Add minor fixes

--- HiCALWeb/compose/local/django/start.sh
+++ HiCALWeb/compose/local/django/start.sh
@@ -14,10 +14,10 @@ python manage.py loaddata hicalweb/topic/fixtures/init_topics.json
 # or via django commands, reading topics.xml file in /topic/fixtures/
 # (update topics/management/commands/import_topics as necessary)
 #python manage.py import_topics
+
 python manage.py collectstatic --no-input
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
 uwsgi --socket 0.0.0.0:8001 --module config.wsgi --master --process 2 --threads 4
-
 
 
 # python manage.py runserver 0.0.0.0:8000
